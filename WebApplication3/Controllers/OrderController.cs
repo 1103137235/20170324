232b60ee31a65eb5ccba47933a84dddb7a08b180
1103137235 <1103137235@gm.kuas.edu.tw>
2017-03-24 15:18:36 +0800

    view

---
+++ WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            Models.OrderService OrderService=new Models.OrderService();
+            var order=OrderService.GetOrderById("111");
+            ViewBag.CustId=order.CustId;
+            ViewBag.CustName=order.CustName;
+            return View();
+        }
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            var result = new Models.Order() { CustId = "GSS", CustName = "叡揚資訊" };
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication3/Models/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class OrderService
+    {
+        public void InsertOrder(Models.Order order)
+        {
+        }
+        public Models.Order GetOrderById(string orderId)
+        {
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚資訊";
+            return result;
+        }
+        public void DeleteOrderById(string orderId)
+        {
+        }
+        public void UpdateOrder(Models.Order order)
+        {
+        }
+    }
+}
